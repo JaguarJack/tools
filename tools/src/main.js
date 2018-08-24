65d41b3573af3e9efbee548b0567f5e8ee27aa8e
yanwenwu <njphper@gmail.com>
2018-08-24 15:48:26 +0800

    添加bootstrap等组件

--- tools/src/main.js
+++ tools/src/main.js
@@ -3,6 +3,15 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import iView from 'iview'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import 'iview/dist/styles/iview.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.min.js'
+
+Vue.use(VueAxios, axios)
+Vue.use(iView)
 
 Vue.config.productionTip = false
 
